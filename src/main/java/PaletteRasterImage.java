2fc898ae171af4ea5eba16fc1a5c88a42583a3d2
Jacquemin-Roman <44777345+Jacquemin-Roman@users.noreply.github.com>
2018-12-10 19:32:42 +0100

    Création Classe PaletteRasterImage

---
+++ src/main/java/PaletteRasterImage.java
@@ -0,0 +1,29 @@
+import image.Image;
+
+public class PaletteRasterImage implements Image {
+
+    public PaletteRasterImage(Color color, int width, int height){
+    }
+    public PaletteRasterImage(Color[][] pixels){
+    }
+
+
+    public void createRepresentation(){
+    }
+    public void setPixelColor(Color color, int x, int y){
+    }
+    public Color getPixelColor(int x, int y){
+    }
+    public void setPixelsColor(Color[][] pixels){
+    }
+    private void setPixelsColor(Color color){
+    }
+    public int getWidth(){
+    }
+    public int getHeight(){
+    }
+    protected void setWidth(int width){
+    }
+    protected void setHeight(int height){
+    }
+}
