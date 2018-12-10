b71d651685e160aa11a4c9db410bc796dc98fe23
Jacquemin-Roman <44777345+Jacquemin-Roman@users.noreply.github.com>
2018-12-10 19:25:57 +0100

    Création Classe BruteRasterImage

---
+++ src/main/java/BruteRasterImage.java
@@ -0,0 +1,30 @@
+import image.Image;
+
+public class BruteRasterImage implements Image {
+
+    public BruteRasterImage(Color color, int width, int height) {
+    }
+    public BruteRasterImage(Color[][] colors){
+    }
+
+
+    public void createRepresentation(){
+    }
+    public void setPixelColor(Color color, int x, int y){
+    }
+    public Color getPixelColor(int x, int y){
+    }
+    private void setPixelsColor(Color[][] pixels){
+    }
+    private void setPixelsColor(Color color){
+    }
+    public int getWidth(){
+    }
+    public int getHeight(){
+    }
+    protected void setWidth(int width){
+    }
+    protected void setHeight(int height){
+    }
+
+}
